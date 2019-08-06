c12a16ce8422e35dc3671292400a87b945b5a5da
CaptainEFFF <davidbanville4@gmail.com>
2019-08-06 18:18:58 -0400

    added assets

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+function(e) {
+    return o.a.createElement("div", {
+        role: "img",
+        "aria-label": "click item",
+        onClick: function() {
+            return e.handleClick(e.id)
+        },
+        style: {
+            backgroundImage: 'url("' + e.image + '")'
+        },
+        className: "click-item" + (e.shake ? " shake" : "")
+    })
+}
+
 serviceWorker.unregister();
